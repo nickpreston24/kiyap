38057bef5bdda7ef08c08e4c7fc32869db15fa31
Michael Preston <michael.n.preston@gmail.com>
2019-03-01 16:04:12 -0600

    Added registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Landing from './pages/Landing'
+import Landing from './pages/Landing';
+import InputForm from './pages/Register';
+import ProSearch from './pages/ProSearch';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Landing/>
-      </div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route component={Landing} exact path='/'></Route>
+                    <Route component={InputForm} exact path='/Register'></Route>
+                    <Route component={ProSearch}></Route>
+                </Switch>
+            </div>
+        </Router>
     );
   }
 }
