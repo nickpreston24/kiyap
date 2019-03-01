97db62b530b0db04d6d3e40301548dd8de9ca4fc
Michael Preston <michael.n.preston@gmail.com>
2019-02-28 22:48:39 -0600

    added client folder

---
+++ client/build/precache-manifest.7a1ff080076b4eb64ce4cf2c819853e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "26500eddd20f9a48a35c",
+    "url": "/static/js/main.26500edd.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "26500eddd20f9a48a35c",
+    "url": "/static/css/main.1a30dd0c.chunk.css"
+  },
+  {
+    "revision": "c9e44b1edf6bf19fb5514964ee7e8035",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/Instructor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Instructor() {
+  return (
+    <div>
+      Hello, Instructor!
+    </div>
+  )
+}
