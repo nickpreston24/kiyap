8dae6fb1bd6e3186734cbd933d703a4929b68c71
Michael Preston <michael.n.preston@gmail.com>
2019-03-10 14:20:30 -0500

    Added new map variants

---
+++ client/src/components/Maps/ControlledZoom.jsx
@@ -0,0 +1,66 @@
+/* global google */
+
+import React from 'react';
+
+// const FaAnchor = require("react-icons/lib/fa/anchor");
+
+import { API_KEY } from '../../constants/keys';
+
+const { compose, withProps, withState, withHandlers } = require("recompose");
+
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} = require("react-google-maps");
+
+const ControlledZoom = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withState('zoom', 'onZoomChange', 8),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    }
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref
+      },
+      onZoomChanged: ({ onZoomChange }) => () => {
+        onZoomChange(refs.map.getZoom())
+      }
+    }
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    zoom={props.zoom}
+    ref={props.onMapMounted}
+    onZoomChanged={props.onZoomChanged}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          {/* <FaAnchor /> */}
+          <i className="fas fa-anchor"></i>
+          {" "}
+          Controlled zoom: {props.zoom}
+        </div>
+      </InfoWindow>
+    </Marker>
+  </GoogleMap>
+);
+
+export default ControlledZoom
