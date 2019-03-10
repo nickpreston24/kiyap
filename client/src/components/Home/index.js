6bd4bd838b19c5c7c3d77a2e34d1c2b1d1c44bfd
Michael Preston <michael.n.preston@gmail.com>
2019-03-10 15:28:45 -0500

    testing different maps

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,19 @@
+
 import React from 'react'
 import { withAuthorization } from '../Session'
+import { LocationSearch, MyMapComponent, MyFancyComponent, MapWithASearchBox, MapWithControlledZoom, PlacesSearchBox } from '../Maps';
 
 const HomePage = () => (
     <div>
         <h1>Home Page</h1>
         <p>Accessible by signed in users</p>
+
+        {/* <LocationSearch/> */}
+        {/* <MyFancyComponent/> */}
+        {/* <PlacesSearchBox/> */}
+        {/* <MyMapComponent isMarkerShown/> */}
+        {/* <MapWithASearchBox/> */}
+        {/* <MapWithControlledZoom/> */}
     </div>
 );
 
