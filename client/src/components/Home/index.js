02f5dabc51ccde09c083b14bce29373e69d55a9b
Michael Preston <michael.n.preston@gmail.com>
2019-03-10 21:52:48 -0500

    chnge

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -12,7 +12,7 @@ const HomePage = () => (
         {/* <MyFancyComponent/> */}
         {/* <PlacesSearchBox/> */}
         {/* <MyMapComponent isMarkerShown/> */}
-        <MapWithASearchBox/>
+        {/* <MapWithASearchBox/> */}
         {/* <MapWithControlledZoom/> */}
     </div>
 );
