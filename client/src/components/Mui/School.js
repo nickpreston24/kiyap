6c7b32176d96f7be67b39ee75348d6e77f0c4f65
Michael Preston <michael.n.preston@gmail.com>
2019-03-15 22:49:58 -0500

    Distinguising Locations and Schools

--- client/src/components/Mui/School.js
+++ client/src/components/Mui/School.js
@@ -7,30 +7,31 @@ import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
 const School = (props) => {
-    console.log('school props: ', props)
     return (
-        props.geometry ? <SchoolLocation {...props}/> : <SavedLocation {...props}/>
+        props.geometry ? <SchoolLocation {...props}/> : <SavedSchool {...props}/>
     )
 }
 
-const SavedLocation = (props) => {
+const SavedSchool = (props) => {
     let {_id, owner, address,
         teaches, website, phone,
-        location: schoolName,
+        name,
         image} = props;
-
-    console.log('geometry:: ', props.geometry)
+    console.log('saved school name', name)
+    // console.log('geometry:: ', props.geometry)
     return(
-        <Card>
+        <Card style={{
+            boxShadow: '0 3px 6px black, 0 3px 6px #e45656',
+            margin: '10px', padding: '3px'}}>
             {image &&
             <CardMedia style={{height: 0, paddingTop: '56.25%'}}
                 image={image}
-                title={schoolName}
+                title={name}
             />}
             <CardContent>
 
                 <Typography gutterBottom variant="headline" component="h2">
-                    {schoolName}
+                    {name}
                 </Typography>
 
                 {teaches &&
@@ -52,33 +53,45 @@ const SavedLocation = (props) => {
 
 //if props have geometry and/or formatted_address, then:
 const SchoolLocation = (props) => {
+    // console.log('school keys: ', Object.keys(props));
+    // console.log('school props: ', props)
+    // console.log('id:', props.place_id)
 
     let {formatted_address:address,
-        name: schoolName,
-        image} = props;
+        name,
+        image,
+        photos,
+        place_id,
+    } = props;
+
+    let {onLike, onDislike} = props;
+    console.log(!!onDislike);
+    console.log('photos: ', props.photos)
 
     return (
-        <Card>
+        <Card style={{
+            boxShadow: '0 3px 6px #e45656, 0 3px 6px #e45656',
+            margin: '10px', padding: '3px'}}>
             {image &&
                 <CardMedia style={{height: 0, paddingTop: '56.25%'}}
                     image={image}
-                    title={schoolName}/>}
+                    title={name}/>}
             <CardContent>
 
                 <Typography gutterBottom variant="headline" component="h2">
-                    {schoolName}
+                    {name}
                 </Typography>
 
                 {address &&
                     <Typography component="p">{address}</Typography>}
 
             </CardContent>
-            {/* <CardActions>
-                <Button size="small" color="primary" href={website} target="_blank">
-                    Go To School Website
-                </Button>
-            </CardActions> */}
+            <CardActions>
+                {/* <h4>Interested?</h4> */}
+                <Button size="small" color="primary" target="_blank" onClick={()=>onLike(place_id)}>Like</Button>
+                <Button size="small" color="secondary" target="_blank" onClick={onDislike}>Dislike</Button>
+            </CardActions>
         </Card>
-)}
+    )}
 
 export default School
