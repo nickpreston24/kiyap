601a2d490c01a56cdacdd50cb60c0093d98cca47
Braden Preston <bpfilmsinc@gmail.com>
2019-03-18 14:42:23 -0500

    Rewired app with decorator support + provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
-import Firebase, {FirebaseContext} from './components/Firebase';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
+import { Provider } from 'mobx-react';
+import { LocationStore } from './components/Stores';
+
+// Initialize Store
+const store = new LocationStore();
+console.log(store)
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
-        <App />
+        <Provider store={store} >
+            <App />
+        </Provider>
     </FirebaseContext.Provider>,
     document.getElementById('root')
 );
