86303be09e9a42de1c8301838b8b5e3483c17ff7
Braden Preston <bpfilmsinc@gmail.com>
2019-03-26 00:18:54 -0500

    Map theme and styling

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,18 +1,68 @@
 import React from 'react'
 import { withAuthorization } from '../Session'
 import { MapWithASearchBox } from '../Maps';
-import {SchoolSearch} from '../Professionals';
-import { LocationStore }  from '../Stores';
+import { SchoolSearch } from '../Professionals';
+import { LocationStore } from '../Stores';
 import { withFlexColumn, withFlexRow } from '../Flex';
+import { withStyles } from '@material-ui/core/styles'
+import { Card } from '@material-ui/core';
 
 const store = new LocationStore();
-const HomePage = () => (
-    <div>
-        <MapWithASearchBox store={store}/>
-        <SchoolSearch store={store}/>
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        minWidth: '100%',
+        maxWidth: '100%',
+        height: '100%',
+        flexFlow: 'row nowrap',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        color: 'red',
+        // border: '2px solid red',
+        overflow: 'hidden',
+    },
+    sidebar: {
+        // margin: '40px 0px',
+        // borderRadius: '8px 0px 0px 8px',
+        display: 'flex',
+        background: theme.palette.primary.main,
+        width: 400,
+        color: 'red',
+        // border: '2px solid green',
+        overflow: 'scroll',
+    },
+    content: {
+        flexShrink: 1,
+        width: 'calc(100% - 400px)',
+        color: 'red',
+        padding: '40px 40px 0px 40px',
+        // border: '2px solid blue',
+        overflow: 'scroll',
+    },
+    maps: {
+        borderRadius: 8,
+        padding: 20,
+        boxSizing: 'border-box',
+        boxShadow: '5px 5px 23px 0px rgba(0,0,0,0.25)',
+        margin: '0px 20px'
+    }
+})
+
+const HomePage = ({ classes }) => (
+    <div className={classes.root}>
+        <div className={classes.content}>
+            <Card className={classes.maps}>
+                <MapWithASearchBox store={store}  />
+            </Card>
+            <SchoolSearch store={store} />
+        </div>
+        <div className={classes.sidebar}>
+            <h1>Hello!</h1>
+        </div>
     </div>
 );
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(HomePage);
+export default withAuthorization(condition)(withStyles(styles)(HomePage));
--- client/src/components/Maps/SearchBox.jsx
+++ client/src/components/Maps/SearchBox.jsx
@@ -15,6 +15,15 @@ const {
 } = require("react-google-maps");
 const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
 // const startCoordinates = {lat: 32.7767, lng: 96.7970 };
+
+function createMapOptions(maps) {
+  return {
+    "styles": [ { "featureType": "all", "elementType": "labels", "stylers": [ { "visibility": "off" }, { "color": "#f49f53" } ] }, { "featureType": "administrative.province", "elementType": "labels.text", "stylers": [ { "visibility": "on" }, { "color": "#ffffff" }, { "saturation": "43" } ] }, { "featureType": "administrative.province", "elementType": "labels.text.fill", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "administrative.locality", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "administrative.locality", "elementType": "labels.text", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative.neighborhood", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "administrative.neighborhood", "elementType": "labels.text", "stylers": [ { "visibility": "off" }, { "hue": "#ff0000" }, { "saturation": "42" } ] }, { "featureType": "landscape", "elementType": "all", "stylers": [ { "color": "#feeada" }, { "lightness": -7 } ] }, { "featureType": "poi.business", "elementType": "all", "stylers": [ { "color": "#645c20" }, { "lightness": 38 } ] }, { "featureType": "poi.government", "elementType": "all", "stylers": [ { "color": "#9e5916" }, { "lightness": 46 } ] }, { "featureType": "poi.medical", "elementType": "geometry.fill", "stylers": [ { "color": "#813033" }, { "lightness": 38 }, { "visibility": "off" } ] }, { "featureType": "poi.park", "elementType": "all", "stylers": [ { "lightness": "-27" }, { "saturation": "-31" }, { "hue": "#aaff00" } ] }, { "featureType": "poi.school", "elementType": "all", "stylers": [ { "color": "#a95521" }, { "lightness": 35 } ] }, { "featureType": "poi.sports_complex", "elementType": "all", "stylers": [ { "color": "#9e5916" }, { "lightness": 32 } ] }, { "featureType": "road", "elementType": "all", "stylers": [ { "color": "#ff1744" }, { "lightness": "20" }, { "visibility": "on" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels.text", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "color": "#ffffff" }, { "weight": "6" } ] }, { "featureType": "road.highway", "elementType": "labels", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road.highway", "elementType": "labels.text", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road.highway", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road.local", "elementType": "geometry.fill", "stylers": [ { "color": "#f19f53" }, { "weight": 1.3 }, { "visibility": "on" }, { "lightness": 16 } ] }, { "featureType": "road.local", "elementType": "geometry.stroke", "stylers": [ { "color": "#f19f53" }, { "lightness": -10 } ] }, { "featureType": "road.local", "elementType": "labels.text", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "visibility": "on" }, { "saturation": "6" }, { "lightness": "-37" } ] }, { "featureType": "road.local", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "color": "#fffcfc" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "lightness": 38 } ] }, { "featureType": "transit.line", "elementType": "all", "stylers": [ { "color": "#813033" }, { "lightness": 22 } ] }, { "featureType": "transit.station", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "color": "#007ba6" }, { "saturation": "-44" }, { "gamma": 0.99 }, { "lightness": 43 } ]
+      }
+    ]
+  }
+}
+
 const MapWithASearchBox = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
@@ -38,10 +47,10 @@ const MapWithASearchBox = compose(
           refs.map = ref;
         },
         onBoundsChanged: () => {
-            // console.log('bounds changed()')
-            // let bnds  = refs.map.getBounds();
-            // let cntr = refs.map.getCenter();
-            // console.log('new bounds: ', bnds, 'new center', cntr);
+          // console.log('bounds changed()')
+          // let bnds  = refs.map.getBounds();
+          // let cntr = refs.map.getCenter();
+          // console.log('new bounds: ', bnds, 'new center', cntr);
           this.setState({
             bounds: refs.map.getBounds(),
             center: refs.map.getCenter(),
@@ -73,44 +82,45 @@ const MapWithASearchBox = compose(
             markers: nextMarkers,
           };
           this.setState(nextState);
-        //   refs.map.fitBounds(bounds);
+          //   refs.map.fitBounds(bounds);
         },
       })
     },
     componentDidMount() {
-        // console.log('wrapper has store?', !!this.props.store)
-        // this.delayedShowMarker()
+      // console.log('wrapper has store?', !!this.props.store)
+      // this.delayedShowMarker()
     },
     componentWillUpdate() {
-        // this.getGeoLocation()
+      // this.getGeoLocation()
     },
     getGeoLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            position => {
-              this.setState({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-              })
-              console.log('geo position: ', position)
-            }
-          )
-        } else {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            this.setState({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            })
+            console.log('geo position: ', position)
+          }
+        )
+      } else {
         //   error => console.log(error)
-        }
       }
+    }
 
   }),
   withScriptjs,
   withGoogleMap
 )(props =>
-    <GoogleMap
+  <GoogleMap
     ref={props.onMapMounted}
     defaultZoom={15}
     center={props.center}
     // center={{ lat: props.lat, lng: props.lng }}
     onBoundsChanged={props.onBoundsChanged}
     clickableIcons={true}
+    options={createMapOptions()}
   >
     <SearchBox
       ref={props.onSearchBoxMounted}
@@ -137,7 +147,7 @@ const MapWithASearchBox = compose(
       />
     </SearchBox>
     {props.markers.map((marker, index) =>
-        <Marker key={index} position={marker.position} />)}
+      <Marker key={index} position={marker.position} />)}
 
   </GoogleMap>
 );
