08cbb49e0bee2542a8b722b9ca7a285548dd3452
Michael Preston <michael.n.preston@gmail.com>
2019-03-27 20:08:35 -0500

    disabled for Demo Day

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -32,9 +32,9 @@ const NavigationAuth = () => (
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
-    <li>
+    {/* <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
+    </li> */}
     <SignOutButton />
   </ul>
 );
