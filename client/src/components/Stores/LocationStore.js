4cb0050c853b80d526a68e579b444059584a8dd3
Michael Preston <michael.n.preston@gmail.com>
2019-03-28 14:51:52 -0500

    Fixed duplicate Schools being saved down.

--- client/src/components/Stores/LocationStore.js
+++ client/src/components/Stores/LocationStore.js
@@ -1,5 +1,5 @@
 import API from '../../utils/API';
-import { decorate, observable, action } from 'mobx';
+import { toJS, decorate, observable, action } from 'mobx';
 
 /**
  * Holds States for School Search components
@@ -28,9 +28,16 @@ export default class LocationStore {
     }
 
     addSchools (locations) {
-        // console.log('locations found:', locations)
-        // console.log('photos', locations.map(l=>l.photos))
-        // console.log('rating', locations.map(l=>l.rating))
+
+        // let currentIds = this.locations.map(l=>l.place_id);
+        // console.log('current location ids: ', currentIds);
+
+        // // let filteredLocations = this.locations.filter(l=>l.place_id)
+        // let newLocations = toJS(locations.filter(l=>!currentIds.includes(l.placed_id)));
+        // console.log('new locations: ',  newLocations);
+        // this.locations = [...newLocations];
+
+        console.log('new locations: ',  locations);
         this.locations = [...locations];
     }
 
@@ -46,11 +53,20 @@ export default class LocationStore {
 
     // indicates user's interest in a school.
     // Professionals will be able to indicate their schools and 'put them on the map' for students to find.
-    saveSchool(id) {
+    saveSchool(lid) {
 
-        let location = this.locations.find(loc=>loc.place_id === id);
+        const schools = toJS(this.schools);
+        // console.log('schools: ', schools)
+        // console.log('new place: ', lid);
+        // console.log('current school ids: ', schools.map(l=>l.placeId));
+        // console.log('dup?', schools.some(school=>school.placeId === lid));
+
+        if(schools.some(school=>school.placeId === lid))
+            return;
+
+        let location = this.locations.find(loc=>loc.place_id === lid);
         let {name, formatted_address: address, place_id} = location;
-        let school = {name, address, studentId: this.studentId};
+        let school = {placeId: lid, name, address, studentId: this.studentId};
 
         this.schools.push(school);
         console.log('saving school: ', school)
@@ -61,12 +77,12 @@ export default class LocationStore {
 
     //Loads the User saved school data, if any
     loadSavedSchools() {
-        // console.log('loaded student id: ', this.studentId)
+        console.log('loaded student id: ', this.studentId)
 
         API.getStudentSchools(this.studentId)
             .then(res => {
                 let data = res.data;
-                // console.log('saved schools: ', data);
+                console.log('saved schools: ', data);
                 this.schools = data;
             })
             .catch(console.error);
