49f72885e5c0b3b4a85d480ddb9a74dbe47e33e6
Michael Preston <michael.n.preston@gmail.com>
2019-06-12 00:18:00 -0500

    build

---
+++ client/build/precache-manifest.7398907eea0e1fd3cfc93a88aa878d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41004e74aa1bbfa6ac50bca26e281337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee1a7d1e37b05a48387c",
+    "url": "/static/css/2.6277bd1e.chunk.css"
+  },
+  {
+    "revision": "0240e3e9689c97bda0db",
+    "url": "/static/css/main.f8eab51e.chunk.css"
+  },
+  {
+    "revision": "ee1a7d1e37b05a48387c",
+    "url": "/static/js/2.e6912bee.chunk.js"
+  },
+  {
+    "revision": "0240e3e9689c97bda0db",
+    "url": "/static/js/main.f58378d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
